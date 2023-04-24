e74e5181fa0171b57f174c888c5ec770a646e1de
Christian Bromann <git@bromann.dev>
2023-04-24 08:55:34 -0700

    make start script more robust - fixes #10

--- .github/scripts/start.js
+++ .github/scripts/start.js
@@ -4,6 +4,8 @@ import path from 'node:path'
 import url from 'node:url'
 import fs from 'node:fs/promises'
 
+import getPort from 'get-port'
+import waitOn from 'wait-on'
 import { remote } from 'webdriverio'
 
 import pkg from '../../package.json' assert { type: 'json' }
@@ -13,19 +15,26 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 /**
  * start browser driver
  */
-async function startDriver (browserName) {
+async function startDriver (port, browserName) {
   const driverName = browserName === 'chrome'
     ? 'chromedriver'
     : 'geckodriver'
+  const hostName = browserName === 'chrome'
+    ? 'localhost'
+    : '0.0.0.0'
   const driver = await import(driverName)
-  driver.start(['--port=4444'])
-  return new Promise((resolve) => setTimeout(resolve, 2000))
+  const p = driver.start([`--port=${port}`])
+  console.log(`Start ${driverName} on port ${port}`)
+  return Promise.race([
+    waitOn({ resources: [`http-get://${hostName}:${port}/status`] }),
+    new Promise((resolve, reject) => p.on('exit', () => reject(new Error(`Failed to start driver`))))
+  ])
 }
 
 /**
  * start WebDriver session with extension installed
  */
-async function startBrowser (browserName) {
+async function startBrowser (port, browserName) {
   const capabilities = browserName === 'chrome'
     ? {
       browserName,
@@ -35,6 +44,7 @@ async function startBrowser (browserName) {
     }
     : { browserName }
   const browser = await remote({
+    port,
     logLevel: 'error',
     automationProtocol: 'webdriver',
     capabilities
@@ -49,6 +59,7 @@ async function startBrowser (browserName) {
 }
 
 const browserName = process.argv.slice(2).pop() || 'chrome'
-console.log(`Run web extension in ${browserName}...`);
-await startDriver(browserName)
-await startBrowser(browserName)
+console.log(`Run web extension in ${browserName}...`)
+const port = await getPort()
+await startDriver(port, browserName)
+await startBrowser(port, browserName)
